7d8fe279d0bdc800b2a76887c16e0e788a4b9399
Viraj Patil <67647998+Virajpatil08@users.noreply.github.com>
2023-08-25 13:30:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+(function ($) {
+    "use strict";
+
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('#nav').addClass('nav-sticky');
+        } else {
+            $('#nav').removeClass('nav-sticky');
+        }
+    });
+
+
+
+
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
+        return false;
+    });
+})
+
+
+
+
+
